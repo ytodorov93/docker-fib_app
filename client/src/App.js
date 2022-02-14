b31f0e2ebf74a68781bb01a64c5c9263e91db81b
ytodorov93 <yordan.todorov@itgix.com>
2022-02-14 12:11:03 +0200

    v5

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Test Change v3</Link>
+          <Link to="/otherpage">Test Change v5</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
